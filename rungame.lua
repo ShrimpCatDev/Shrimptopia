6fbac978e078649bd91f74397fc50c195e8a2a2d
ShrimpCatDev <shrimpcatboi223@outlook.com>
2025-09-24 17:54:21 -0500

    added palette filtering

--- rungame.lua
+++ rungame.lua
@@ -2,6 +2,8 @@ local game={}
 shove.createLayer("game")
 
 function game:enter()
+    fade=0
+    car=lg.newImage("assets/car.png")
     currentGame="test"
     local code,size = love.filesystem.read("games/"..currentGame.."/run.lua")
 
@@ -16,18 +18,37 @@ function game:enter()
 
     func()
     env.init()
+
+    local lutEffect=love.graphics.newShader("assets/shader/lut.glsl")
+
+    pal=lg.newImage("assets/lut.png")
+    pal:setFilter("nearest","nearest")
+
+    lutEffect:send("lut",pal)
+
+    shove.removeEffect("game",lutEffect)
+    shove.addEffect("game",lutEffect)
 end
 
 function game:update(dt)
     env.update(dt)
+    --=fade=fade+dt*0.1
+    if input:pressed("a") then
+        fade=0
+    end
 end
 
 function game:draw()
+    --lg.setShader(lutEffect)
     shove.beginDraw()
         shove.beginLayer("game")
             env.draw()
+            lg.draw(car,0,0)
+            lg.setColor(0,0,0,fade)
+            lg.rectangle("fill",0,0,96,64)
         shove.endLayer()
     shove.endDraw()
+    --lg.setShader()
 end
 
 return game
